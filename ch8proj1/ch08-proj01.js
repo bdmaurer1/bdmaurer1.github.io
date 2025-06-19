687fa6510e6e767b588365698e0a7debe42c0cca
Brady Maurer <bdmaurer@svsu.edu>
2025-06-19 18:02:26 -0400

    ch8proj1 initial changes

---
+++ ch8proj1/ch08-proj01.js
@@ -0,0 +1,25 @@
+
+const tax_rate = parseFloat(prompt('Enter tax rate (e.g. 0.10)'));
+const shipping_threshold = parseFloat(prompt('Enter shipping threshold (e.g. 1000)'));
+
+let subtotal = 0;
+
+for (let i = 0; i < cart.length; i++) {
+  let item = cart[i];
+  let total = calculateTotal(item.quantity, item.product.price);
+  subtotal += total;
+  outputCartRow(item, total);
+}
+
+let tax = subtotal * tax_rate;
+let shipping = subtotal > shipping_threshold ? 0 : 40;
+let grandTotal = subtotal + tax + shipping;
+
+document.write(`<tr><td colspan="4">Subtotal</td><td>$${subtotal.toFixed(2)}</td></tr>`);
+document.write(`<tr><td colspan="4">Tax</td><td>$${tax.toFixed(2)}</td></tr>`);
+document.write(`<tr><td colspan="4">Shipping</td><td>$${shipping.toFixed(2)}</td></tr>`);
+document.write(`<tr><td colspan="4">Grand Total</td><td>$${grandTotal.toFixed(2)}</td></tr>`);
+
+
+/* add loop and other code here ... in this simple exercise we are not
+   going to concern ourselves with minimizing globals, etc */
---
+++ ch8proj1/functions.js
@@ -0,0 +1,37 @@
+function calculateTotal(quantity, price) {
+  return quantity * price;
+}
+function outputCartRow(item, total) {
+  document.write("<tr>");
+  document.write(`<td><img src='images/${item.product.filename}'></td>`);
+  document.write(`<td>${item.product.title}</td>`);
+  document.write(`<td>${item.quantity}</td>`);
+  document.write(`<td>$${item.product.price.toFixed(2)}</td>`);
+  document.write(`<td>$${total.toFixed(2)}</td>`);
+  document.write("</tr>");
+}
+let subtotal = 0;
+
+for (let i = 0; i < cart.length; i++) {
+  let item = cart[i];
+  let total = calculateTotal(item.quantity, item.product.price);
+  subtotal += total;
+  outputCartRow(item, total);
+}
+
+let tax = subtotal * 0.1; 
+let grandTotal = subtotal + tax;
+
+document.write(`<tr><td colspan="4">Subtotal</td><td>$${subtotal.toFixed(2)}</td></tr>`);
+document.write(`<tr><td colspan="4">Tax</td><td>$${tax.toFixed(2)}</td></tr>`);
+document.write(`<tr><td colspan="4">Total</td><td>$${grandTotal.toFixed(2)}</td></tr>`);
+
+
+
+
+
+
+
+
+
+        
