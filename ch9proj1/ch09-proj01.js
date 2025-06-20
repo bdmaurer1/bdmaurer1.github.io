f6620048b1a6075b9657dedc14ad176d3d19b28a
Brady Maurer <bdmaurer@svsu.edu>
2025-06-20 19:18:46 -0400

    ch9 initial changes

---
+++ ch9proj1/ch09-proj01.js
@@ -0,0 +1,47 @@
+const symbolPlay = '⯈';
+const symbolPause = '❚ ❚';
+const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
+const aside = document.querySelector("aside");
+const video = document.querySelector("video");
+
+files.forEach(file => {
+  const img = document.createElement("img");
+  img.src = `images/${file}.jpg`;
+  img.alt = file;
+  
+  // When image is clicked, change video source
+  img.addEventListener("click", () => {
+    video.pause();
+    video.src = `videos/${file}.mp4`;
+    video.load();
+    video.play();
+    playPauseBtn.textContent = symbolPause;
+  });
+
+  aside.appendChild(img);
+});
+const stopBtn = document.querySelector("#stopBtn");
+const playPauseBtn = document.querySelector("#playPauseBtn");
+
+stopBtn.addEventListener("click", () => {
+  video.pause();
+  video.currentTime = 0;
+  playPauseBtn.textContent = symbolPlay;
+});
+const skipButtons = document.querySelectorAll("[data-skip]");
+
+skipButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    video.currentTime += parseFloat(button.dataset.skip);
+  });
+});
+playPauseBtn.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playPauseBtn.textContent = symbolPause;
+  } else {
+    video.pause();
+    playPauseBtn.textContent = symbolPlay;
+  }
+});
+
